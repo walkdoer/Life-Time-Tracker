a50e287408ba4a7bc65729487108465b967c0cd4
zhangmh <zhangmhao@gmail.com>
2014-08-07 23:09:47 +0800

    根据class进行分组

--- stat/statists/stat_day.js
+++ stat/statists/stat_day.js
@@ -1,11 +1,10 @@
-
 'use strict';
 
 var util = require('../util');
 var msg = require('../message');
 var helper = require('./helper');
 
-exports.stat = function (dateArr) {
+exports.stat = function(dateArr) {
     var year = parseInt(dateArr[0]);
     var month = parseInt(dateArr[1]);
     var day = parseInt(dateArr[2]);
@@ -14,7 +13,8 @@ exports.stat = function (dateArr) {
     }
     util.readLogFiles(year, month, day)
         .then(analyse)
-        .catch(handleError);
+        .
+    catch (handleError);
 };
 
 
@@ -29,29 +29,42 @@ function analyse(result) {
         tagNum: tags.length,
         logNum: logs.length
     }));
-    msg.log('tags:'.bold + tags.join(', ').italic.blue);
-    msg.log('classes:'.bold + classes.join(', ').magenta);
+    msg.log('Tags: '.bold + tags.join(', ').italic.blue);
+    msg.log('Classes: '.bold + classes.join(', ').magenta);
 
     //calculate total time
     var totalMins = 0;
-    logs.forEach(function (log, index) {
+    var logInfoArr = [];
+    logs.forEach(function(log, index) {
         var logInfo = helper.getLogInfo(log, date, index);
         if (logInfo) {
             if (isGetUpLog(logInfo)) {
+                logInfo.getup = true;
                 msg.info('Get Up Time: ' + logInfo.start);
             }
             if (logInfo.len !== undefined) {
                 totalMins += logInfo.len;
-            } else if(!logInfo.start){
+            } else if (!logInfo.start) {
+                logInfo.len = 0;
                 msg.warn('May be there\' something wrong with you time format of this log: ' +
                     log);
             }
+            logInfoArr.push(logInfo);
         }
     });
     var totalHours = totalMins / 60;
-
     msg.log('total time: ' + totalMins.toString().cyan + ' minutes; ' + totalHours.toFixed(2).cyan + ' hours.');
+
+    //output every classes time consume
+    msg.log('========== Group By Classes =========='.grey);
+    classes.forEach(function(cls) {
+        var consumeTime = calculateClassesTimeConsume(logInfoArr, cls);
+        msg.log(cls.bold + ':' + (consumeTime / 60).toFixed(2).cyan + ' hours');
+    });
+
 }
+
+
 
 function getBasicInfo(data) {
     return data.date + ' have ' + data.logNum + ' logs and ' + data.tagNum + ' tags;';
@@ -64,9 +77,20 @@ function isGetUpLog(log) {
 function handleError(err) {
     if (err.code === 'ENOENT') {
         msg.error('can\' find log file ' + err.path +
-                ', please check the existence of the file');
+            ', please check the existence of the file');
     } else {
         msg.error(err.message);
         throw err;
     }
+}
+
+function calculateClassesTimeConsume(logInfoArr, cls) {
+    var totalTime = 0;
+    logInfoArr.forEach(function(log) {
+        var classes = log.classes;
+        if (classes && classes.indexOf(cls) >= 0) {
+            totalTime += log.len;
+        }
+    });
+    return totalTime;
 }
