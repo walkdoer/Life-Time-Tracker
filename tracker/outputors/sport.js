df9fd4a1eda76801e214f6f30bc261c8904f173a
zhangmh <zhangmhao@gmail.com>
2014-08-22 18:35:18 +0800

    bugfix

--- tracker/outputors/sport.js
+++ tracker/outputors/sport.js
@@ -8,7 +8,7 @@ var display = require('../dislpay_data');
 
 exports.dispose = function (result) {
     preprocessResult(result);
-    console.log('健身次数:' + result.count);
+    console.log('运动次数:' + result.count);
     console.log('一共花了' + (result.time / 60).toFixed(1) + 'h 在健身');
 
     console.log('\n======= 按照健身项目分类=========\n');
