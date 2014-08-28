aaec1c9ab4174672b65ba8039766bb6c510de5b6
zhangmh <zhangmhao@gmail.com>
2014-08-28 16:14:37 +0800

    完善perspective

--- tracker/statists/month.js
+++ tracker/statists/month.js
@@ -10,168 +10,36 @@
 
 'use strict';
 
-var moment = require('moment');
-var extend = require('node.extend');
-var dayStat = require('./day');
+
 var util = require('../util');
-var msg = require('../message');
 var logClassEnum = require('../enum/logClass');
+var logClassMap = util.inversObj(logClassEnum);
+var overviewPerspective = require('./perspectives/monthOverview');
+var dayStat = require('./day');
+var extend = require('node.extend');
 
 exports.dispose = function(options, scanResult) {
-    var days = scanResult.days,
-        dayNum = days.length,
-        sleepPeriodArr = [],
-        unTrackedTimes = [];
+    var statResult,
+        perspective,
+        perspectives;
 
-
-    /**
-     * record sleep period
-     */
-    days = days.map(function (d) {
-        var dayOptions = extend({}, d, {dateStr: d.date});
+    scanResult.days = scanResult.days.map(function (d) {
+        var dayOptions = extend({}, options, {dateStr: d.date});
         d = dayStat.dispose(dayOptions, d);
-        recordSleepPeriod(d);
-        recordUnTrackedTime(d);
         return d;
     });
-
-
-    /**
-     * 记录睡眠周期
-     *
-     */
-    function recordSleepPeriod(data) {
-        if (!data.sleepMoment) {
-            msg.error(data.date + 'have no sleepTime');
-        }
-        if (!data.wakeMoment) {
-            msg.error(data.date + 'have no wakeTime');
-        }
-        sleepPeriodArr.push({
-            date: data.date,
-            sleepMoment: new moment(data.sleepMoment),
-            wakeMoment: new moment(data.wakeMoment),
-            sleepTime: data.sleepTime
-        });
+    if (options.logClass) {
+        perspective = logClassMap[options.logClass];
+        perspectives = [perspective];
+    } else {
+        statResult = overviewPerspective.focus(scanResult);
+        perspectives = ['sport'];
     }
-
-    /**
-     * 记录每一天没有跟踪到的时间
-     */
-    function recordUnTrackedTime(day) {
-        unTrackedTimes.push({
-            label: day.date,
-            count: day.unTrackedTime
-        });
-    }
-
-    var classTime = groupTimeByClass(days).sort(desc),
-        tagTime = groupTimeByTag(days).sort(desc),
-        projectTime = groupTimeBy('project', days).sort(desc);
-
-    var meanSleepTime = util.mean(days.filter(function (d) {
-        return d.sleepTime > 0;
-    }), 'sleepTime');
-    var sumTimeResult = sumTime(days);
-
-    //calculate the mean value for all logClasses
-    var meanValues = {};
-    Object.keys(logClassEnum).forEach(function (key) {
-        meanValues['mean' + key + 'Time'] = mean(classTime, dayNum, logClassEnum[key]);
+    perspectives.forEach(function (key) {
+        console.log(key);
     });
-    return extend({
-        days: days,
-        unTrackedDays: scanResult.unTrackedDays,
-        unTrackedTimes: unTrackedTimes,
-        sleepPeriodArr: sleepPeriodArr,
-        //mean sleep time of a month
-        meanSleepTime: meanSleepTime,
-        classTime: classTime,
-        tagTime: tagTime,
-        projectTime: projectTime
-    }, sumTimeResult, meanValues);
 
-    function desc(a, b) {
-        return b.count - a.count;
-    }
+    return statResult;
+
 };
 
-function groupTimeByTag(days) {
-    return groupTimeBy('tag', days);
-}
-
-function groupTimeBy(type, days, process, filter) {
-    var result = [];
-    filter = filter || function (item, groupItem) {
-        return item.label === groupItem.label;
-    };
-    days.forEach(function (d) {
-        var times = d[type + 'Time'];
-        times.forEach(function (time) {
-            var target = result.filter(filter.bind(null, time));
-            if (target && target.length > 0) {
-                target[0].count += time.count;
-            } else {
-                if(typeof process === 'function') {
-                    time = process(extend({}, time));
-                }
-                result.push(time);
-            }
-        });
-    });
-
-
-    return result;
-}
-
-function groupTimeByClass(days) {
-    var result = [];
-    days.forEach(function (d) {
-        var classTime = d.classTime;
-        classTime.forEach(function (t) {
-            var target = getTarget(t.code);
-
-            if (target) {
-                target.count += t.count;
-            } else {
-                result.push(t);
-            }
-        });
-    });
-
-    function getTarget(code) {
-        var target = result.filter(function (itm) {
-            return itm.code === code;
-        });
-
-        return target[0] || null;
-    }
-
-    return result;
-}
-
-
-function sumTime(days) {
-    var sum = days.reduce(function (sum, d) {
-        sum.trackedTime += d.trackedTime;
-        sum.sleepTime += d.sleepTime;
-        sum.activeTime += d.activeTime;
-        sum.unTrackedTime += d.unTrackedTime;
-        return sum;
-    }, {
-        sleepTime: 0,
-        trackedTime: 0,
-        activeTime: 0,
-        unTrackedTime: 0
-    });
-    return sum;
-}
-
-
-function mean(data, len, logClass) {
-    var total = data.filter(function (d) {
-        return d.code === logClass;
-    })[0];
-
-    return total.count / len;
-}
