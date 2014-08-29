92a7a9264728dcaa8e46557db1ef912b30345f25
zhangmh <zhangmhao@gmail.com>
2014-08-29 17:40:58 +0800

    添加坐站的统计

--- tracker/statists/perspectives/sit.js
+++ tracker/statists/perspectives/sit.js
@@ -3,5 +3,36 @@
  *
  * see how many time I sit one day or one period,like one week or one month.
  */
+'use strict';
+
+var logClassEnum = require('../../enum/logClass');
+
+exports.focus = function (options, scanResult) {
+    var logs = scanResult.logs;
+
+    logs.forEach(function (log) {
+        if (isStandLog(log)) {
+            console.log('');
+        }
+    });
+};
+
+function isStandLog(log) {
+    var classes = log.classes,
+        tags = log.tags;
+    return hasLogClass(classes, logClassEnum.Sport) ||
+        (hasLogClass(classes, logClassEnum.NormalThing) && hasTag(tags, '信息漫游'));
+}
 
 
+function hasLogClass(logClasses, targetCls) {
+    return logClasses.filter(function (cls) {
+        return cls.code === targetCls;
+    }).length > 0;
+}
+
+function hasTag(tags, targetTag) {
+    return tags.filter(function (tag) {
+        return tag === targetTag;
+    }).length > 0;
+}
