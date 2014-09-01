b0737a0a532e41ba2fcc46ba6baf7284fce50073
zhangmh <zhangmhao@gmail.com>
2014-09-01 18:37:06 +0800

    统计运动日历

--- tracker/calendars/sportCalendar.js
+++ tracker/calendars/sportCalendar.js
@@ -5,9 +5,35 @@
 'use strict';
 
 var scanner = require('../scanner');
+
+var logClassEnum = require('../enum/logClass');
 exports.generate = function(options) {
     scanner.scan(options)
-        .then(function(scanResult) {
-            console.log(scanResult);
-        });
+        .then(generateCalendar.bind(null, options));
 };
+
+function generateCalendar(options, scanResult) {
+    var result = [];
+    scanResult.days.forEach(function (day) {
+        var logs = day.logs;
+        var sportTime = 0;
+        logs.forEach(function (log){
+            if (isSportLog(log)) {
+                sportTime += log.len;
+            }
+        });
+        result.push({
+            date: day.date,
+            sportTime: sportTime
+        });
+    });
+    console.log(result);
+}
+
+
+function isSportLog(log) {
+    var classes = log.classes;
+    return classes && classes.filter(function (cls) {
+        return cls.code === logClassEnum.Sport;
+    }).length > 0;
+}
