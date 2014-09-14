2a2e3b65b54b50e57247c740e61f711c6d5f702f
zhangmh <zhangmhao@gmail.com>
2014-09-14 22:47:47 +0800

    展示平均时间统计

--- tracker/statists/perspectives/month.mean.js
+++ tracker/statists/perspectives/month.mean.js
@@ -8,13 +8,11 @@
 'use strict';
 
 exports.focus = function (options, scanResult, statResult) {
-    //calculate the mean value for all logClasses
     var dayNum = statResult.days.length,
         meanClassTime,
         meanTagTime,
         meanProjectTime;
 
-    //calculate the mean value for all logClasses
     meanClassTime = mean(statResult.classTime, dayNum);
     meanProjectTime = mean(statResult.projectTime, dayNum);
     meanTagTime = mean(statResult.tagTime, dayNum);
