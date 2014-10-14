f1451fce1ead7128a5ecb84b6da4e92cff56059b
zhangmh <zhangmhao@gmail.com>
2014-10-14 18:35:45 +0800

    完善数据查询

--- tracker/search/search.js
+++ tracker/search/search.js
@@ -8,14 +8,17 @@
 
 var Q = require('q');
 var Log = require('../model/log');
+var TimeFormat = require('../timeFormat');
+var dateTypeEnum = require('../enum/dateType');
+var Moment = require('moment');
 
-exports.query = function (options) {
+exports.query = function(options) {
     var deferred = Q.defer();
-    queryLog(options, function (result) {
-        deferred.resolve(result.map(function (item) {
+    queryLog(options, function(result) {
+        deferred.resolve(result.map(function(item) {
             return item.toJSON();
         }));
-    }, function (err) {
+    }, function(err) {
         deferred.reject(err);
     });
     return deferred.promise;
@@ -23,12 +26,44 @@ exports.query = function (options) {
 
 
 function queryLog(options, onSuccess, onError) {
-
-    Log.find(function (err, result) {
+    var condition = createQueryCondition(options);
+    var args = [condition, function(err, result) {
         if (err) {
             onError(err);
         } else {
             onSuccess(result);
         }
-    });
+    }].filter(function (val) { return !!val;});
+    Log.find.apply(Log, args);
+}
+
+
+function createQueryCondition(options) {
+    var queryCondition = {};
+    var dateCondition = getDateCondition(options);
+    if (dateCondition) {
+        queryCondition.date = dateCondition;
+    }
+    return queryCondition;
+}
+
+function getDateCondition(options) {
+    var condition,
+        zero = ' 00:00:00',
+        date = options.dateItems[0];
+    if (date) {
+        if (date.type === dateTypeEnum.Day) {
+            condition = new Date(date.value + zero);
+        } else if (date.type === dateTypeEnum.Month) {
+            var m = new Moment(date.value);
+            var startDate = m.startOf('month').format(TimeFormat.date),
+                endDate = m.endOf('month').format(TimeFormat.date);
+            condition = {
+                $gte: new Date(startDate + zero),
+                $lt: new Date(endDate + zero)
+            };
+        }
+    }
+
+    return condition;
 }
