25024395b7ea50536891177f616cad44b221ccfd
zhangmh <zhangmhao@gmail.com>
2014-11-07 18:15:34 +0800

    cal-heatmap

--- client-browser/js/components/charts/CalendarHeatMap.js
+++ client-browser/js/components/charts/CalendarHeatMap.js
@@ -1,68 +1,68 @@
-define(function(require) {
-    'use strict';
-    var React = require('react');
-    var d3 = require('d3');
-    var moment = require('moment');
-    var CalHeatMap = require('app/libs/cal-heatmap');
-    var R = React.DOM;
-    var Q = require('q');
-    var className = 'ltt_c-calendarHeapMap';
-    var _ = require('underscore');
-    var LoadIndicator = require('../loadIndicator');
+var React = require('react');
+var d3 = require('d3');
+var moment = require('moment');
+var Q = require('q');
+var className = 'ltt_c-calendarHeapMap';
+var CalHeatMap = require('../../libs/cal-heatmap');
+var _ = require('lodash');
+var LoadIndicator = require('../LoadIndicator');
 
-    var CalendarHeatMap = React.createClass({
-        displayName: 'calendarHeatMap',
-        getInitialState: function () {
-            return {
-                msg: 'loading'
-            };
-        },
-        componentDidMount: function () {
-            var that = this;
-            createCalHealMap(this.props.url, this.props.options)
-                .then(function () {
-                    that.refs.ind.done();
-                });
-        },
-        render: function() {
-            return R.div({className: className}, LoadIndicator({ref: 'ind'}));
-        }
-    });
-
-     function createCalHealMap(url, options) {
-        var deferred = Q.defer();
-        var calendar = new CalHeatMap();
-        d3.json(url, function(error, data) {
-            deferred.resolve(data);
-            var renderData = {};
-            data.forEach(function(val) {
-                var seconds = new Date(val.date).getTime() / 1000;
-                if (val.sportTime > 0) {
-                    renderData[seconds] = val.sportTime;
-                }
+var CalendarHeatMap = React.createClass({
+    displayName: 'calendarHeatMap',
+    getInitialState: function () {
+        return {
+            msg: 'loading'
+        };
+    },
+    componentDidMount: function () {
+        var that = this;
+        createCalHealMap(this.props.url, this.props.options)
+            .then(function () {
+                that.refs.ind.done();
             });
-
-            var defaulOptions = {
-                itemSelector: '.' + className,
-                data: renderData,
-                start: new Date(2014, 0),
-                domain: "month",
-                subDomain: "day",
-                //subDomainTextFormat: "%d",
-                cellSize: 12,
-                cellPadding: 1,
-                tooltip: true,
-                subDomainTitleFormat: {
-                    empty: 'No data'
-                },
-                subDomainDateFormat: function(date) {
-                    return moment(date).format('D号 dddd');
-                }
-            };
-            calendar.init(_.extend({}, defaulOptions, options));
-        });
-        return deferred.promise;
+    },
+    render: function() {
+        return (
+            <div className={className}>
+                <LoadIndicator  ref='ind'/>
+            </div>
+        );
     }
-
-    return CalendarHeatMap;
 });
+
+ function createCalHealMap(url, options) {
+    var deferred = Q.defer();
+    var calendar = new CalHeatMap();
+    d3.json(url, function(error, data) {
+        deferred.resolve(data);
+        var renderData = {};
+        data.forEach(function(val) {
+            var seconds = new Date(val.date).getTime() / 1000;
+            if (val.sportTime > 0) {
+                renderData[seconds] = val.sportTime;
+            }
+        });
+
+        var defaulOptions = {
+            itemSelector: '.' + className,
+            data: renderData,
+            start: new Date(2014, 0),
+            domain: "month",
+            subDomain: "day",
+            //subDomainTextFormat: "%d",
+            cellSize: 12,
+            cellPadding: 1,
+            tooltip: true,
+            subDomainTitleFormat: {
+                empty: 'No data'
+            },
+            subDomainDateFormat: function(date) {
+                return moment(date).format('D号 dddd');
+            }
+        };
+        calendar.init(_.extend({}, defaulOptions, options));
+    });
+    return deferred.promise;
+}
+
+module.exports = CalendarHeatMap;
--- client-browser/js/pages/Dashboard.js
+++ client-browser/js/pages/Dashboard.js
@@ -3,13 +3,19 @@
  */
 
 var React = require('react');
-
+var CalendarHeatMap = require('../components/charts/calendarHeatMap');
 var Dashboard = React.createClass({
 
     render: function () {
         return (
             <div className="ltt_c-page-dashboard">
-            dashboard
+                <p>Dashboard</p>
+                <CalendarHeatMap
+                    url="/api/calendars/sport/2014"
+                    options= {{
+                        empty: "no sport data",
+                        filled: "{date} 运动时间 {count}分钟"
+                    }}/>
             </div>
         );
     }
