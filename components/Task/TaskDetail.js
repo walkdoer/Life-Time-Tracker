3d07a162985097da2f121db240c6ea25cc1a18cf
zhangmh <zhangmhao@gmail.com>
2015-03-28 00:01:03 +0800

    bugfix

--- components/Task/TaskDetail.js
+++ components/Task/TaskDetail.js
@@ -24,8 +24,10 @@ var DataAPI = require('../../utils/DataAPI');
 module.exports = React.createClass({
 
     getInitialState: function () {
+        var task = this.props.task;
         return {
             loaded: false,
+            estimatedTime: task ? task.estimatedTime : null,
             logs: []
         };
     },
@@ -41,9 +43,9 @@ module.exports = React.createClass({
 
         return (
             <aside className="ltt_c-projectTask-logs">
-                <div className="ltt_c-LogList">
+                <div className="ltt_c-LogList" key={task._id}>
                     <div className="ltt_c-LogList-header">
-                        <input type="text" placeHolder="filter log" className="searchInput"/>
+                        <span className="searchInput">{task.name}</span>
                         <span className="closeWindow" onClick={this.hide} title="close">
                             <i className="fa fa-close"></i>
                         </span>
@@ -52,7 +54,10 @@ module.exports = React.createClass({
                         <Row className="ltt_c-taskDetail-dateInfo-item">
                             <Col xs={6} className="ltt_c-taskDetail-dateInfo-item-label" md={4}>Estimated</Col>
                             <Col xs={12} className="ltt_c-taskDetail-dateInfo-item-input" md={8}>
-                                <input type="text" name="estimatedTime" ref="estimatedTime" onChange={this.updateEstimatedTime} value={task.estimatedTime}/>
+                                <input type="text" name="estimatedTime" ref="estimatedTime"
+                                    onKeyDown={this.onEstimatedTimeKeyDown}
+                                    onChange={this.updateEstimatedTime}
+                                    value={buildTime(this.state.estimatedTime)}/>
                             </Col>
                         </Row>
                         <Row className="ltt_c-taskDetail-dateInfo-item">
@@ -95,29 +100,65 @@ module.exports = React.createClass({
         DataAPI.Task.update(changedValue).then(function(result) {
             console.info('success', result);
         }).catch(function (err) {
-            console.log(err);
             Notify.error('fail to change date');
         });
     },
 
-    updateEstimatedTime: function (e) {
-        var value = e.target.value;
+    onEstimatedTimeKeyDown: function (e) {
+        var ENTER = 13;
+        if( e.keyCode == ENTER ) {
+            this.updateEstimatedTime(e.target.value);
+        }
+    },
+
+    updateEstimatedTime: function (time) {
+        var time = this.inspectTime(time);
+        var that = this;
+        if (time === undefined) {return;}
         DataAPI.Task.update({
             id: this.props.task._id,
-            estimatedTime: parseInt(value, 10)
+            estimatedTime: time
         }).then(function(result) {
-            console.info('success', result);
+            console.info('success', result.estimatedTime);
+            that.setState({
+                estimatedTime: result.estimatedTime
+            });
         }).catch(function (err) {
-            console.log(err);
             Notify.error('fail to updateEstimatedTime');
         });
     },
 
+
+    inspectTime: function (time) {
+        if (!time) {
+            return;
+        }
+        var unit = getDateUnit(time);
+        var result;
+        if (unit) {
+            var value = parseFloat(time);
+            if (unit === 'minute') {
+                result = value;
+            } else if(unit === 'hour') {
+                result = value * 60;
+            }  else if(unit === 'month') {
+                result = value * 30 * 24 * 60;
+            } else if (unit === 'year') {
+                result = value * 356 * 24 * 60;
+            } else if (unit === 'day') {
+                result = value * 24 * 60;
+            }
+            return result;
+        }
+    },
+
     componentWillReceiveProps: function (nextProps) {
         var that = this;
+        var task = nextProps.task;
         if (!_.isEqual(nextProps, this.props)) {
             this.setState({
-                loaded: false
+                loaded: false,
+                estimatedTime: task ? task.estimatedTime : null
             }, function () {
                 that.load();
             });
@@ -150,6 +191,77 @@ module.exports = React.createClass({
     }
 });
 
+function buildTime(time) {
+    if (!time) {
+        return;
+    }
+    var month, day, hour;
+    var hour = time / 60;
+    if (hour < 1) {
+        return number(time) + ' minutes';
+    } else if (hour < 24) {
+        return number(hour) + ' hours';
+    } else if (hour >= 24) {
+        day = hour / 24;
+        if (day > 1 && day < 30) {
+            return number(day) + ' days';
+        } else if (day >= 30){
+            month = day / 30;
+            if (month > 1 && month < 12) {
+                return number(month) + ' months';
+            } else if (month >= 12) {
+                year = month / 12;
+                return number(year) + ' years';
+            }
+        }
+    }
+}
+
+function isInt(n){
+    return Number(n)===n && n%1===0;
+}
+
+function number(val) {
+    if(!isInt(val)) {
+        return val.toFixed(1);
+    }
+    return val;
+}
+
+function getDateUnit(time) {
+    if (!time) {
+        return;
+    }
+    var dateWords = ['year', 'month', 'day', 'week', 'hour', 'minute'];
+    var arr = time.split(/\s+/);
+    var unitStr = arr[1];
+    var unit;
+    if (!unitStr) {
+        return 'minute';
+    }
+    if (unitStr === 'm') {
+        return 'minute';
+    } else if (unitStr === 'M') {
+        return 'month';
+    } else if (unitStr.toLowerCase() === 'min') {
+        return 'minute';
+    } else if (unitStr.toLowerCase() === 'mon') {
+        return 'month';
+    }
+    dateWords.some(function (word) {
+        var firstLetter = word[0];
+        if (word === unitStr ||
+            firstLetter === unitStr ||
+            firstLetter.toUpperCase() === unitStr ||
+            (word + 's') === unitStr ||
+            word === unitStr.toLowerCase()
+        ) {
+            unit = word;
+        }
+    })
+    return unit;
+}
+
 var EMPTY_FUN = function () {};
 var DateTimePicker = React.createClass({
 
@@ -173,20 +285,33 @@ var DateTimePicker = React.createClass({
         var that = this;
         var $el = $(this.getDOMNode());
         $el.datetimepicker().on('dp.hide', function (e) {
-            console.log('dp.hide');
             var date = e.date;
             that.props.onChange({
                 name: that.props.name,
                 date: date.toDate()
             });
         }).on('dp.change', function (e) {
-            console.log('dp.change');
             var date = e.date;
             that.props.onChange({
                 name: that.props.name,
                 date: date.toDate()
             });
         });
-        $el.data('DateTimePicker').date(new Moment(this.props.value).toDate());
+        if (this.props.value) {
+            $el.data('DateTimePicker').date(new Moment(this.props.value).toDate());
+        } else {
+            $el.data('DateTimePicker').date(null);
+        }
+    },
+
+    componentWillReceiveProps: function (nextProps) {
+        console.log('receive');
+        var dateTimePicker = $(this.getDOMNode()).data('DateTimePicker');
+        if (!dateTimePicker) { return; }
+        if (nextProps.value) {
+            dateTimePicker.date(new Moment(nextProps.value).toDate());
+        } else {
+            dateTimePicker.date(null);
+        }
     }
 });
