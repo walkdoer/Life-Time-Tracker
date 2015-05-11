dee2787c333e690ad52aea5dd4c9e62079044f0b
zhangmh <zhangmhao@gmail.com>
2015-05-11 13:57:18 +0800

    add lib

---
+++ bower_components/Highstock-2.1.5/exporting-server/java/highcharts-export/highcharts-export-web/target/tmp/jsp/org/apache/jsp/WEB_002dINF/pages/error_jsp.java
@@ -0,0 +1,80 @@
+package org.apache.jsp.WEB_002dINF.pages;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class error_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html; charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("<head>\n");
+      out.write("<meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("<title>Highcharts export server</title>\n");
+      out.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"resources/css/demo.css\" />\n");
+      out.write("</head>\n");
+      out.write("<body>\n");
+      out.write("\t<div id=\"top\">\n");
+      out.write("        <a href=\"http://www.highcharts.com\" title=\"Highcharts Home Page\"\n");
+      out.write("           id=\"logo\"><img alt=\"Highcharts Home Page\"\n");
+      out.write("                          src=\"http://www.highcharts.com/templates/highsoft_colorful/images/logo.svg\" border=\"0\"></a>\n");
+      out.write("    </div>\n");
+      out.write("\t<div id=\"wrap\">\n");
+      out.write("\t\t<h3>Oops..,</h3>\n");
+      out.write("\t\t<p>");
+      out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${message}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+      out.write("</p>\n");
+      out.write("\t</div>\n");
+      out.write("</body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
